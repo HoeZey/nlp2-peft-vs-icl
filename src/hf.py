dc08234f267d262b84be608814efdc38b5b79de4
Hoshe Lee <hoshelee@ymail.com>
2025-04-25 16:16:45 +0200

    Added path arg

--- src/hf.py
+++ src/hf.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 from huggingface_hub import login
 
 
-def hf_login():
-    load_dotenv("./secrets.env")
+def hf_login(path: str = "./secrets.env"):
+    load_dotenv(path)
     login(token=os.environ.get("HF_ACCESS_TOKEN"))
