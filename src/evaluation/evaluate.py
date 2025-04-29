a8ce448eaf1c64a8724716eedfb6e05f07e041f3
HoeZey <hoshelee@ymail.com>
2025-04-29 17:10:45 +0200

    Implemented zs and fs evaluation

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+import re
 import torch
 from torch.utils.data import DataLoader, RandomSampler
 from vllm import LLM, SamplingParams
@@ -17,8 +20,17 @@ class FewshotSampler:
         return [self.dataset[i] for i in self.sampler]
 
 
-def extract_answer(generated: list[str]) -> torch.IntTensor:
-    return torch.tensor([int(g.split("####")[-1]) for g in generated], dtype=int)
+def extract_answer(g: str):
+    answer = re.findall(r"\d+", g)
+    return (
+        min(int(answer[-1]), 999_999_999_999)
+        if answer is not None and len(answer) > 0
+        else -1
+    )
+
+
+def extract_answers(generated: list[str]) -> torch.IntTensor:
+    return torch.tensor([extract_answer(g) for g in generated], dtype=int)
 
 
 def evaluate(
@@ -26,32 +38,60 @@ def evaluate(
     sampling_params: SamplingParams,
     eval_dataset: Dataset,
     batch_size: int,
+    answers_file: Path,
+    output_file: Path,
     fewshot_sampler: RandomSampler = None,
     lora_request: LoRARequest = None,
     system_prompt: str = "",
 ):
     correct = 0
+    model_answers: dict[int, int] = (
+        json.load(answers_file.open("r")) if answers_file.stat().st_size > 0 else dict()
+    )
+    model_output: dict[int, int] = (
+        json.load(output_file.open("r")) if output_file.stat().st_size > 0 else dict()
+    )
 
-    for questions in DataLoader(eval_dataset, batch_size=batch_size, shuffle=False):
-        solutions = extract_answer([q["answer"] for q in questions])
+    for batch, questions in enumerate(
+        DataLoader(eval_dataset, batch_size=batch_size, shuffle=False)
+    ):
+        if str((batch + 1) * batch_size - 1) in model_answers:
+            pass
+        print(f"Processing batch {batch}")
+
+        solutions = extract_answers(questions["answer"])
         prompts = []
-        for q in questions:
-            prompt = system_prompt
+        for q in questions["question"]:
+            prompt = f"{system_prompt}\n\n"
             if fewshot_sampler:
-                for i, example in enumerate(fewshot_sampler.sample()):
-                    prompt += f"Example {i + 1}\nQuestion:\n"
-                    prompt += example["question"] + "\nAnswer:\n"
-                    prompt += example["answer"] + "\n\n"
-            prompt += "Test:\nQuestion:\n" + q["question"]
+                for batch, example in enumerate(fewshot_sampler.sample()):
+                    # prompt += f"Example {batch + 1}\n"
+                    prompt += f"Q: {example['question']}\n"
+                    split = example["answer"].split(" #### ")
+                    prompt += f"A: {split[0]} The answer is {split[1]}\n\n"
+            prompt += f"Q: {q}\nA: "
             prompts.append(prompt)
 
         outputs = model.generate(
-            questions,
+            prompts,
             sampling_params,
             lora_request=lora_request,
         )
-        answers = extract_answer(outputs[0].outputs[0].text)
-
+        generated = [o.outputs[0].text for o in outputs]
+        answers = extract_answers(generated)
         correct += (answers == solutions).sum()
+
+        for i, g in enumerate(generated):
+            model_output[str(batch * batch_size + i)] = g
+
+        print(f"Solutions: {solutions.tolist()}")
+        print(f"Answers: {answers.tolist()}")
+
+        for i, (a, g) in enumerate(zip(answers.tolist(), generated)):
+            model_answers[str(batch * batch_size + i)] = a
+            model_output[str(batch * batch_size + i)] = g
+
+        json.dump(model_answers, answers_file.open("w"), indent=4)
+        json.dump(model_output, output_file.open("w"), indent=4)
 
     print(f"Accuracy: {100 * correct / len(eval_dataset):.2f}")
--- src/main_eval.py
+++ src/main_eval.py
@@ -1,4 +1,6 @@
+import json
 import argparse
+from pathlib import Path
 from vllm import LLM, SamplingParams
 from vllm.lora.request import LoRARequest
 from datasets import load_dataset
@@ -11,13 +13,16 @@ def parse_args():
 
     # Model
     parser.add_argument("--model_path", type=str)
-    parser.add_argument("--system_prompt_path", type=str)
+    parser.add_argument("--system_prompt_path", type=str, default=None)
     parser.add_argument("--lora_path", type=str, default=None)
+    parser.add_argument("--num_gpus", type=int, default=1)
 
     # Datasets
     parser.add_argument("--eval_dataset", type=str)
+    parser.add_argument("--eval_dataset_config_name", type=str)
     parser.add_argument("--eval_split", type=str, default="test")
-    parser.add_argument("--icl_datset", type=str, default=None)
+    parser.add_argument("--icl_dataset", type=str, default=None)
+    parser.add_argument("--icl_dataset_config_name", type=str, default=None)
     parser.add_argument("--icl_split", type=str, default="train")
     parser.add_argument("--k", type=int, default=0)
 
@@ -28,32 +33,57 @@ def parse_args():
     # parser.add_argument("--output_dir", type=str, default="./outputs/evaluation")
     parser.add_argument("--seed", type=int, default=42)
 
+    parser.add_argument("--output_dir", type=str)
+
     return parser.parse_args()
 
 
 def main():
     hf_login()
     args = parse_args()
-    model = LLM(args.model_path)
+    print(args)
+
+    model = LLM(args.model_path, tensor_parallel_size=args.num_gpus)
     sampling_params = SamplingParams(
         max_tokens=args.max_tokens,
     )
     lora_request = LoRARequest(args.lora_path) if args.lora_path else None
-    eval_dataset = load_dataset(args.eval_dataset, split=args.eval_split)
+    eval_dataset = load_dataset(
+        args.eval_dataset, name=args.eval_dataset_config_name, split=args.eval_split
+    )
 
     sampler = None
     if args.k > 0:
-        icl_dataset = load_dataset(args.icl_dataset, split=args.icl_split)
+        icl_dataset = load_dataset(
+            args.icl_dataset, name=args.icl_dataset_config_name, split=args.icl_split
+        )
         sampler = FewshotSampler(icl_dataset, k=args.k, seed=args.seed)
 
-    with open(args.system_prompt_path, "r") as f:
-        system_prompt = "\n".join(f.readlines())
+    if args.system_prompt_path is not None:
+        with open(args.system_prompt_path, "r") as f:
+            system_prompt = "\n".join(f.readlines())
+    else:
+        system_prompt = ""
+
+    output_dir = Path(args.output_dir)
+    answers_file = f"{'_'.join(args.model_path.split('/'))}_k={args.k}_answers.json"
+    output_file = f"{'_'.join(args.model_path.split('/'))}_k={args.k}_output.json"
+
+    if not (output_dir / answers_file).exists():
+        output_dir.mkdir(exist_ok=True, parents=True)
+        json.dump({}, (output_dir / answers_file).open("w"))
+
+    if not (output_dir / output_file).exists():
+        output_dir.mkdir(exist_ok=True, parents=True)
+        json.dump({}, (output_dir / output_file).open("w"))
 
     evaluate(
         model,
         sampling_params,
         eval_dataset=eval_dataset,
         batch_size=args.batch_size,
+        answers_file=output_dir / answers_file,
+        output_file=output_dir / output_file,
         fewshot_sampler=sampler,
         lora_request=lora_request,
         system_prompt=system_prompt,
