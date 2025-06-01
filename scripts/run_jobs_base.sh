10ede7f942ab68e619403faad0f1629fb931ec98
HoeZey <hoshelee@ymail.com>
2025-06-01 19:21:09 +0200

    Added jobs

--- scripts/run_jobs_base.sh
+++ scripts/run_jobs_base.sh
@@ -1,19 +1,15 @@
 #!/bin/bash          
-WAIT=5
+WAIT=10  # wait between jobs because scheduling jobs too fast causes errors
 
 # Llama3.1-Base
 sbatch jobs/llama3_base/gsm8k/run_eval_llama3_0s.job
 sleep $WAIT
 sbatch jobs/llama3_base/gsm8k/random/run_eval_llama3_1s.job
 sleep $WAIT
-# sbatch jobs/llama3_base/gsm8k/random/run_eval_llama3_2s.job
-# sleep $WAIT
 sbatch jobs/llama3_base/gsm8k/random/run_eval_llama3_4s.job
 sleep $WAIT
 sbatch jobs/llama3_base/gsm8k/sim/run_eval_llama3_1s.job
 sleep $WAIT
-# sbatch jobs/llama3_base/gsm8k/sim/run_eval_llama3_2s.job
-# sleep $WAIT
 sbatch jobs/llama3_base/gsm8k/sim/run_eval_llama3_4s.job
 sleep $WAIT
 
@@ -21,12 +17,8 @@ sbatch jobs/llama3_base/math/run_eval_llama3_0s.job
 sleep $WAIT
 sbatch jobs/llama3_base/math/random/run_eval_llama3_1s.job
 sleep $WAIT
-# sbatch jobs/llama3_base/math/random/run_eval_llama3_2s.job
-# sleep $WAIT
 sbatch jobs/llama3_base/math/random/run_eval_llama3_4s.job
 sleep $WAIT
 sbatch jobs/llama3_base/math/sim/run_eval_llama3_1s.job
 sleep $WAIT
-# sbatch jobs/llama3_base/math/sim/run_eval_llama3_2s.job
-# sleep $WAIT
 sbatch jobs/llama3_base/math/sim/run_eval_llama3_4s.job
